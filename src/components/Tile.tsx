76c2f5ebc952c32107f89438a681e61cfefb1444
Lillie Schachter <lillieschachter@gmail.com>
2023-06-02 08:11:44 -0400

    add Tile component

---
+++ src/components/Tile.tsx
@@ -0,0 +1,18 @@
+import classNames from "classnames";
+
+import "./Tile.css";
+
+type Props = {
+  color_1: string;
+  color_2: string;
+  onClick(): void;
+};
+
+export default function Tile({ color_1, color_2, onClick }: Props) {
+  return (
+    <div className="tile" onClick={onClick}>
+      <div className={classNames("left", color_1)}></div>
+      <div className={classNames("right", color_2)}></div>
+    </div>
+  );
+}
